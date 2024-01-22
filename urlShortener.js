29d4d3b45f86b6bc04c1a3a232326b4f3a1ab213
EdisonMeks <edisonmeks@gmail.com>
2024-01-22 12:03:59 +0300

    Added a url shortener file

---
+++ urlShortener.js
@@ -0,0 +1,29 @@
+// Information to reach API
+const apiKey = '008efd2d30a5479caece13e4c90c0b84';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// Asynchronous functions
+const shortenUrl = () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination:urlToShorten});
+  fetch(url, {method:'POST', headers:{
+  'Content-type': 'application/json',
+  'apikey': apiKey
+}, body:data});
+}
+
+// Clear page and call Asynchronous functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
